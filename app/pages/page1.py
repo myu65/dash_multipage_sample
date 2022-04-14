3da4b14ffb0be810acd0c174b555bb94f8f25ccc
myu65 <myu.laurant@gmail.com>
2022-04-14 22:29:27 +0900

    idを変数化

--- app/pages/page1.py
+++ app/pages/page1.py
@@ -1,18 +1,20 @@
 from dash import dcc, html, Input, Output, callback
 
+PAGE_ID = 'page1'
+
 layout = html.Div([
     html.H3('Page 1'),
     dcc.Dropdown(
         {f'Page 1 - {i}': f'{i}' for i in ['New York City', 'Montreal', 'Los Angeles']},
-        id='dropdown'
+        id= f'{PAGE_ID}_dropdown'
     ),
-    html.Div(id='display-value'),
+    html.Div(id=f'{PAGE_ID}_display-value'),
     dcc.Link('Go to Page 2', href='/page2')
 ])
 
 
 @callback(
-    Output('display-value', 'children'),
-    Input('dropdown', 'value'))
+    Output(f'{PAGE_ID}_display-value', 'children'),
+    Input(f'{PAGE_ID}_dropdown', 'value'))
 def display_value(value):
     return f'You have selected {value}'
--- app/pages/page2.py
+++ app/pages/page2.py
@@ -1,18 +1,20 @@
 from dash import dcc, html, Input, Output, callback
 
+PAGE_ID = 'page2'
+
 layout = html.Div([
     html.H3('Page 2'),
     dcc.Dropdown(
         {f'Page 2 - {i}': f'{i}' for i in ['London', 'Berlin', 'Paris']},
-        id='dropdown'
+        id=f'{PAGE_ID}_dropdown'
     ),
-    html.Div(id='display-value'),
+    html.Div(id=f'{PAGE_ID}_display-value'),
     dcc.Link('Go to Page 1', href='/page1')
 ])
 
 
 @callback(
-    Output('display-value', 'children'),
-    Input('dropdown', 'value'))
+    Output(f'{PAGE_ID}_display-value', 'children'),
+    Input(f'{PAGE_ID}_dropdown', 'value'))
 def display_value(value):
     return f'You have selected {value}'
